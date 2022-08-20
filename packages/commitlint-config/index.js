4e4b9f3857338e62b112e6e8aab8d8dc4ffc3795
StimulCross <stimulcross@gmail.com>
2022-08-20 16:47:48 +0600

    feat: add commitlint-config

---
+++ packages/commitlint-config/index.js
@@ -0,0 +1,19 @@
+/**
+ * strvcom/commitlint-config
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2019 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict';
+
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+
+	rules: {
+		'body-leading-blank': [2, 'always'],
+		'body-max-line-length': [0],
+		'footer-max-line-length': [0]
+	}
+};
---
+++ packages/commitlint-config/lerna.js
@@ -0,0 +1,13 @@
+/**
+ * strvcom/commitlint-config
+ *
+ * @author      Robert Rossmann <rr.rossmann@me.com>
+ * @copyright   2019 STRV
+ * @license     http://choosealicense.com/licenses/bsd-3-clause  BSD-3-Clause License
+ */
+
+'use strict';
+
+module.exports = {
+	extends: ['./index.js', '@commitlint/config-lerna-scopes']
+};
